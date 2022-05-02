f42bd0f2b0d9c412a1e0d738c2ead323e5eb0453
VeronicaMSilva <veronica.maria8793@gmail.com>
2022-05-02 16:49:29 -0300

    class 07 exercises

--- javaulas/src/logica/aulas/aula07/Aula07Exercicios.java
+++ javaulas/src/logica/aulas/aula07/Aula07Exercicios.java
@@ -7,7 +7,7 @@ public class Aula07Exercicios {
 	public static void main(String[] args) {
 		
 	Scanner tec = new Scanner (System.in);
-		
+	/*	
 	System.out.println(".............Atividade 04............."); //ultilizando "do/while"
 		double nota1, nota2; 
 		do {
@@ -21,8 +21,8 @@ public class Aula07Exercicios {
 		}while (nota2 < 0 || nota2 > 10);
 		double media = (nota1 + nota2) / 2;
 		System.out.println("Sua média é: " + media);
-		
-		
+	*/	
+	/*	
 	System.out.println("");
 	System.out.println(".............Exercicio 1.............");
 		String opcao;
@@ -31,19 +31,43 @@ public class Aula07Exercicios {
 			System.out.println("Deseja exibir o texto novamente?");
 			opcao = tec.nextLine();
 		}while (opcao.contains("sim"));
-		
+	*/	
+	/*
 	System.out.println("");
 	System.out.println(".............Exercicio 2.............");
 		for (int i=0; i <= 100; i+=10) {
 			System.out.println(i);
 		}
-		
-		
-		
-	
-		
-		
-			
+	*/
+	/*
+	System.out.println("");
+	System.out.println(".............Exercicio 3.............");
+	Scanner tectec = new Scanner (System.in);
+	int num = tectec.nextInt();
+	int vezes = 0;
+	do {	
+		System.out.println(num + " x " + (vezes) + " = " + (num * vezes));
+		vezes++;
+	} while (vezes <= 25);
+	*/
+	/*
+	System.out.println("");
+	System.out.println(".............Exercicio 4.............");	
+	Scanner entrada = new Scanner (System.in);
+	int valores;
+	int total = 0;
+	int rept = 0;
+	do {
+		System.out.print("digite um número: ");
+		valores = entrada.nextInt();
+		total = valores + total;
+		rept++;
+	}while (rept <= 9);
+	System.out.println("A soma dos valores digitados são: " + total);
+	*/
+	System.out.println("");
+	System.out.println(".............Exercicio 4.............");	
+	Scanner entrada = new Scanner (System.in);
 		
 		
 			
